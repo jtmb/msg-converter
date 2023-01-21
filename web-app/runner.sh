b9899c1cb8c65e39e8d36544d765b0edb915a414
jtmb <james.branco@gmail.com>
2023-01-21 16:28:44 -0500

    updated entry point

--- web-app/runner.sh
+++ web-app/runner.sh
@@ -1,3 +1,5 @@
 sudo docker compose -f "./nginx/docker-compose.yml" up -d --build
 # sudo docker exec nginx-php-1 adduser root www-data && chown -R www-data:www-data /var/www && chmod -R g+rwX /var/www && \
 # sudo docker exec nginx-php-1 apk add py3-pip && pip install extract-msg
+
+adduser root /home/james/container-program-files/msg-converter/web-app/nginx/frontend && chmod -R g+rwX /home/james/container-program-files/msg-converter/web-app/nginx/frontend
