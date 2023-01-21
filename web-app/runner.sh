c41a83b553a0175ab0e3e01dccac14dd9b6274c8
jtmb <james.branco@gmail.com>
2023-01-21 16:57:25 -0500

    changes

--- web-app/runner.sh
+++ web-app/runner.sh
@@ -1,2 +1,2 @@
 sudo docker compose -f "./nginx/docker-compose.yml" up -d --build
-# sudo docker exec nginx-php-1 adduser root www-data && chown -R www-data:www-data /var/www && chmod -R g+rwX /var/www && \
+sudo docker exec nginx-php-1 sh /usr/local/bin/entrypoint
