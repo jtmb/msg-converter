685c0995e9c87f177e8076fb688c1c530ba0c06e
jtmb <james.branco@gmail.com>
2023-01-21 10:28:18 -0500

    changes to front end

--- web-app/nginx/frontend/index.php
+++ web-app/nginx/frontend/index.php
@@ -1,12 +1,36 @@
 <!DOCTYPE html>
 <html>
+<head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<link rel="stylesheet" href="style.css">
 <body>
 
-<form action="upload.php" method="post" enctype="multipart/form-data">
-  Select file to upload:
-  <input type="file" name="fileToUpload" id="fileToUpload">
-  <input type="submit" value="Upload File" name="submit">
-</form>
+<div class="bg"></div>
+<div class="bg bg2"></div>
+<div class="bg bg3"></div>
 
+<p></p>
+
+  <div class="center">
+  <h1>.MSG to HTML file Converter</h1>
+  <div class="actions-box">
+  <form action="upload.php" method="post" enctype="multipart/form-data">
+  <p class="p3">Select file to upload:</p>
+    <p></p>
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <p></p>
+    <input type="submit" value="Upload & Convert File" name="submit">
+  </form>
+  </div>
+
+
+  </div>
+
+  <div class="navbar">
+  <a href="#home" class="active">Home</a>
+  <a href="https://github.com/jtmb/msg-converter">Repo</a>
+  <a href="#contact">About</a>
+</div>
 </body>
 </html>
