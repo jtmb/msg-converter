f2c561dc7af76a7bfec727dd3baf2b73fe3f731f
jtmb <james.branco@gmail.com>
2023-01-21 15:56:32 -0500

    woops

--- web-app/runner.sh
+++ web-app/runner.sh
@@ -1,3 +1,3 @@
-docker compose -f "./nginx/docker-compose.yml" up -d --build && \
+docker compose -f "./nginx/docker-compose.yml" up -d --build
 # sudo docker exec nginx-php-1 adduser root www-data && chown -R www-data:www-data /var/www && chmod -R g+rwX /var/www && \
 # sudo docker exec nginx-php-1 apk add py3-pip && pip install extract-msg
