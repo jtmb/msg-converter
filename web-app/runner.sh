841bc8d1f311f31c35df2125f7fd92a772e99328
jtmb <james.branco@gmail.com>
2023-01-23 17:29:19 -0500

    updates

--- web-app/runner.sh
+++ web-app/runner.sh
@@ -1,7 +1,8 @@
 # var to generate random string and set as db_pwd
 db_pwd=`(tr -dc A-Za-z0-9 </dev/urandom | head -c 13 ; echo '')`
-sudo rm - nginx/.env
 sudo echo "DB_PWD=$db_pwd" > nginx/.env
+mkdir ~/tmp/ && sudo echo -e "DB_PWD=$db_pwd" > ~/tmp/.env
+sudo mv ~/tmp/.env ~/container-program-files/msg-converter/web-app/nginx/.env
 
 # Build docker compose stack and set entry point script
 sudo docker compose -p "msg-converter" -f "./nginx/docker-compose.yml" up -d --build
