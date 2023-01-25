bdfe998517538fc840df0cc93e22fe40342f095e
jtmb <james.branco@gmail.com>
2023-01-24 19:44:25 -0500

    test

---
+++ web-app/nginx/frontend/loader.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<link rel="stylesheet" href="style.css">
+<body>
+
+<div class="bg"></div>
+<div class="bg bg2"></div>
+<div class="bg bg3"></div>
+
+<p></p>
+  <form action="upload.php" method="post" enctype="multipart/form-data">
+  <div class="loader"></div>
+  </form>
+
+  <!-- <div class="submit-div"><input type="file" name="fileToUpload" id="fileToUpload"> <p></p><input type="submit" value="Upload & Convert File" name="submit"></div> -->
+
+
+
+  <div class="navbar">
+  <a href="/" class="active">Home</a>
+  <a href="https://github.com/jtmb/msg-converter">Repo</a>
+  <a href="/about.html">About</a></div>
+</body>
+</html>
