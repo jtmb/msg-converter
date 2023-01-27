28b914f4d425825014065df1ba3db2ea920fcbb8
jtmb <james.branco@gmail.com>
2023-01-27 14:22:04 -0500

    moved scripts into js folder

--- web-app/nginx/frontend/index.php
+++ web-app/nginx/frontend/index.php
@@ -20,7 +20,7 @@
   <div class="submit-div"><input type="file" name="fileToUpload" id="fileToUpload"> <p></p>
   <input type="submit" value="Upload & Convert File" name="submit" id="search-button"></div>
   </form>
-<script src="/loading.js"></script>
+<script src="/js/loading.js"></script>
   
   <div class="navbar">
   <a href="/" class="active">Home</a>
