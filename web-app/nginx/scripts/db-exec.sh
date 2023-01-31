a6e16dd80bd8c971d576febd1ee9192005f909ff
jtmb <james.branco@gmail.com>
2023-01-30 19:17:52 -0500

    db prep
    Co-authored-by: iserve4u@gmail.com <iserve4u@gmail.com>

--- web-app/nginx/scripts/db-exec.sh
+++ web-app/nginx/scripts/db-exec.sh
@@ -1,4 +1,31 @@
 #!/bin/sh
 
-# database integration information 
+# VARS
+    
+    # declare database password variable         containerpath # /usr/local/bin/.env
+    db_pwd=$(docker exec -it msg-converter-php-1 cat ~/repos/msg-converter/web-app/nginx/.env | rev | cut -d "=" -f1 | rev)
+
+    #count number of lines in logs
+    ln=$(docker exec -it msg-converter-php-1 wc -l /var/www/html/logs/logs.out | cut -d " " -f1)
+
+    if [[$ln -gt 0]]; then
+        wkln=1p
+        name=$(docker exec -it msg-converter-php-1 sed -n $wkln /var/www/html/logs/logs.out | cut -d"|" -f1)
+        date=$(docker exec -it msg-converter-php-1 sed -n $wkln /var/www/html/logs/logs.out | cut -d "|" -f3)
+        ip=$(docker exec -it msg-converter-php-1 sed -n $wkln /var/www/html/logs/logs.out | cut -d "|" -f2)
+    else
+
+    echo "--------- debug --------------"
+    echo $wkln
+    echo $name
+    echo $date
+    echo $ip
+    echo "----------------end debug--------------"
+
+
+
+
+
+# cat log files
+docker exec -it msg-converter-php-1 cat /var/www/html/logs/logs.out
 
--- web-app/runner.sh
+++ web-app/runner.sh
@@ -5,7 +5,7 @@ mkdir ~/tmp/ && sudo echo -e "DB_PWD=$db_pwd" > ~/tmp/.env
 sudo mv ~/tmp/.env ~/container-program-files/msg-converter/web-app/nginx/.env
 
 # Build docker compose stack and set entry point script
-sudo docker compose -p "msg-converter" -f "./nginx/docker-compose.yml" up -d --build
+sudo docker compose --env-file nginx/.env -p "msg-converter" -f "./nginx/docker-compose.yml" up -d --build
 sudo docker exec msg-converter-php-1 sh /usr/local/bin/entrypoint
 
 # Show random generated db_pwd at the end of build
