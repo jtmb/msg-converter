33d216dc60deea20731336e3c0c7a1967b2d6f13
jtmb <james.branco@gmail.com>
2023-02-01 16:22:17 -0500

    chnaged db to sql

--- web-app/runner.sh
+++ web-app/runner.sh
@@ -9,6 +9,8 @@ sudo docker compose --env-file nginx/.env -p "msg-converter" -f "./nginx/docker-
 sudo docker exec msg-converter-php-1 sh /usr/local/bin/entrypoint
 
 # Show random generated db_pwd at the end of build
+db_pwd=$(cat ~/repos/msg-converter/web-app/nginx/.env | rev | cut -d "=" -f1 | rev)
+
 echo
 echo "-----------------------"
 echo db login info: 
