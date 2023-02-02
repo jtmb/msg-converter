81ba3b60bd00713a8a5f1416fbedbe802f351e94
jtmb <james.branco@gmail.com>
2023-02-02 15:39:24 -0500

    updated style

--- web-app/nginx/frontend/about/about.php
+++ web-app/nginx/frontend/about/about.php
@@ -3,7 +3,7 @@
 <head> 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
-<link rel="stylesheet" href="./about_style.css">
+<link rel="stylesheet" href="../css/about_style.css">
 
 <style></style>
 <body>
