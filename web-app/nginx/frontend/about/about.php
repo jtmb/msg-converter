c76d2caef654388c10ca22cb71569b93666018a8
jtmb <james.branco@gmail.com>
2023-02-02 15:24:39 -0500

    fixed about UI

--- web-app/nginx/frontend/about/about.php
+++ web-app/nginx/frontend/about/about.php
@@ -4,7 +4,19 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <link rel="stylesheet" href="./about_style.css">
+
 <style></style>
+<body>
+
+<div class="topnav">
+  <a href="/">Home</a>
+  <a href="https://github.com/jtmb/msg-converter">Repo</a>
+  <a class="active" href="/about/about.php">About</a>
+  <div class="topnav-right">
+    <!-- <a href="index.php">Modern View</a> -->
+    <a href="./classic/index.php">Classic View</a>
+  </div>
+</div>
 
 
 <div class="txt">
@@ -15,13 +27,11 @@
       <div class="about3"> <h2>More comming soon!</h2></div>
 </div>
 
-</section>
+
   <div class="navbar">
     <div class=social>
       <div class=fa><a href="https://www.linkedin.com/in/james-monteiro-branco-0ab7a688?originalSubdomain=ca" class="fa fa-linkedin"></a>
       <a href="https://github.com/jtmb" class="fa fa-github"></a></div></div>
-    <a href="/">Home</a>
-  <a href="https://github.com/jtmb/msg-converter">Repo</a>
-  <a href="/about/about.html"class="active">About</a>
+    <a href="/"></a>
 </body>
 </html>
