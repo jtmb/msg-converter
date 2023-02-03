aedcc5b84a4a5138d6fcbd547c617bb6617511b7
jtmb <james.branco@gmail.com>
2023-02-03 18:07:27 -0500

    updated uploads

--- web-app/nginx/frontend/classic/upload.php
+++ web-app/nginx/frontend/classic/upload.php
@@ -32,7 +32,7 @@ if ($uploadOk == 0) {
   if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
 
     // echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
-    $output = shell_exec('python3 -m extract_msg /var/www/html/'.escapeshellarg($target_file).' --out ./converted-messages/uploads/ --prepared-html --use-filename --html ');
+    $output = shell_exec('python3 -m extract_msg /var/www/html/'.escapeshellarg($target_file).' --out /var/www/html/converted-messages/uploads/ --prepared-html --use-filename --html ');
     $name = trim($target_file,".msg!"); //trim output
     $output2 = file_get_contents( "/var/www/html/converted-messages$name/message.html" ); // get the contents, and echo it out.
     if (str_contains($output2, 'Warning: file_get_contents')) {
